d5c2264fd6c57940e5fcf5adc31622555990dad8
Javier Quinteros <javier@quinteros.eu>
2017-11-18 17:44:11 +0100

    Update the repository.

--- sc3microapi.py
+++ sc3microapi.py
@@ -29,7 +29,7 @@
 import cherrypy
 import os
 import json
-from typing import Dict, List, Tuple
+import MySQLdb
 
 try:
     import configparser
@@ -37,81 +37,33 @@ except ImportError:
     import ConfigParser as configparser
 
 
-class TemplatesAPI(object):
-    """Object dispatching methods related to templates."""
+class NetworksAPI(object):
+    """Object dispatching methods related to networks."""
 
-    def __init__(self, directory: str):
+    def __init__(self):
         """Constructor of the IngestAPI class."""
-        self.directory = directory
-        self.extension = 'n3'
+        pass
 
-    def retrieve(self, template: str, params: dict) -> str:
-        """Fill a template with the parameters passed.
+    def index(self):
+        """List available networks in the system.
 
-        :returns: Complete template in text plain format.
-        :rtype: string
-        :raises: FileNotFoundError, BadRequest
-        """
-
-        prefix: str = 'EUDAT_PARAM'
-
-        with open(self.directory + '/' + template + '.' + self.extension) as fin:
-            # Keep the specification in a list
-            wholetemp: str = fin.read()
-            for key, value in params.items():
-                wholetemp = wholetemp.replace('{%s:%s}' % (prefix, key), value)
-
-            # Check that all variables have been replaced
-
-            # Look for opening markup of variable
-            startvar: int = wholetemp.find('{%s:' % prefix)
-            if startvar >= 0:
-                # Look for closing markup of variable
-                endvar: int = wholetemp.find('}', startvar)
-                if endvar >= startvar:
-                    raise Exception('Missing variable: %s' % wholetemp[startvar+len(prefix)+2:endvar])
-
-            return wholetemp
-
-    def list(self) -> str:
-        """List available templates in the system.
-
-        :returns: Metadata related to the available templates in JSON format.
+        :returns: Data related to the available networks.
         :rtype: string
         :raises: cherrypy.HTTPError
         """
         try:
-            templates: List[str] = []
-            for (dirpath, dirnames, filenames) in os.walk(self.directory):
-                # Filter out files not ending with self.extension
-                filenames = [fi[:-len(self.extension)-1] for fi in filenames if fi.endswith('.' + self.extension)]
-                templates.extend(filenames)
-                break
+            templates = []
         except:
             # Send Error 404
-            messDict: Dict = {'code': 0,
-                              'message': 'Could not read the list of available templates'}
-            message: str = json.dumps(messDict)
+            messDict = {'code': 0,
+                        'message': 'Could not read the list of available templates'}
+            message = json.dumps(messDict)
             cherrypy.log(message, traceback=True)
             cherrypy.response.headers['Content-Type'] = 'application/json'
             raise cherrypy.HTTPError(404, message)
 
         # Save the templates specification to be returned
-        result: List[Dict[str, str]] = list()
-        for template in templates:
-            # Open template
-            with open(self.directory + '/' + template + '.' + self.extension) as fin:
-                # Keep the specification in a list
-                docstring: List[str] = list()
-                for line in fin.readlines():
-                    if line.startswith('#'):
-                        # Add all lines starting with #
-                        docstring.append(line[1:])
-                    else:
-                        # Break as soon as line doesn't start with #
-                        break
-                result.append({'name': template, 'doc': '\n'.join(docstring)})
-
+        result = list()
         return json.dumps(result)
 
 
@@ -121,89 +73,56 @@ class SC3MicroApi(object):
     def __init__(self):
         """Constructor of the Provgen object."""
         config = configparser.RawConfigParser()
-        here: str = os.path.dirname(__file__)
+        here = os.path.dirname(__file__)
         config.read(os.path.join(here, 'sc3microapi.cfg'))
 
         # Read connection parameters
-        self.templatesAPI = TemplatesAPI(config.get('Service', 'templatesdir'))
+        self.network = NetworksAPI()
 
     @cherrypy.expose
-    def index(self) -> str:
-        cherrypy.response.header_list = [('Content-Type', 'text/html')]
+    def index(self):
+        cherrypy.response.header['Content-Type'] = 'text/html'
 
         # TODO Create an HTML page with a minimum documentation for a user
         try:
             with open('help.html') as fin:
-                texthelp: str = fin.read()
+                texthelp = fin.read()
         except:
-            texthelp: str = """<html>
-                                <head>sc3microapi</head>
-                                <body>
-                                  Default help for the sc3microapi service (GEOFON).
-                                </body>
-                              </html>"""
+            texthelp = """<html>
+                            <head>sc3microapi</head>
+                            <body>
+                              Default help for the sc3microapi service (GEOFON).
+                            </body>
+                          </html>"""
 
         return texthelp
 
     @cherrypy.expose
-    def templates(self, *args: Tuple, **kwargs: Dict) -> str:
-        """Get a template in Notation3 format or a list of templates (JSON).
-
-        :returns: Templates available in JSON format or a template with all variables replaced.
-        :rtype: string
-        """
-        if not len(args):
-            cherrypy.response.header_list = [('Content-Type', 'application/json')]
-            return self.templatesAPI.list()
-
-        cherrypy.response.header_list = [('Content-Type', 'text/n3')]
-        try:
-            result = self.templatesAPI.retrieve('/'.join(args), kwargs)
-            return result
-        except FileNotFoundError:
-            # Send Error 404
-            messDict: Dict = {'code': 0,
-                              'message': 'Template %s could not be found.' %
-                                         '/'.join(args)}
-            message: str = json.dumps(messDict)
-            # cherrypy.log(message)
-            cherrypy.response.headers['Content-Type'] = 'application/json'
-            raise cherrypy.HTTPError(404, message)
-        except Exception as e:
-            # Send Error 400
-            messDict: Dict = {'code': 0,
-                              'message': str(e)}
-            message: str = json.dumps(messDict)
-            # cherrypy.log(message)
-            cherrypy.response.headers['Content-Type'] = 'application/json'
-            raise cherrypy.HTTPError(400, message)
-
-    @cherrypy.expose
-    def features(self) -> str:
-        """Read the features of the system and return them in JSON format.
-
-        :returns: System capabilities in JSON format
-        :rtype: string
-        """
-        syscapab: Dict = {
-                     "whatever": False,
-                   }
-        cherrypy.response.header_list = [('Content-Type', 'application/json')]
-        return json.dumps(syscapab)
-
-    @cherrypy.expose
-    def version(self) -> str:
+    def version(self):
         """Return the version of this implementation.
 
-        :returns: System capabilities in JSON format
+        :returns: Version of the system
         :rtype: string
         """
-        version: str = '0.1a1'
-        cherrypy.response.header_list = [('Content-Type', 'text/plain')]
+        version = '0.1a1'
+        cherrypy.response.header['Content-Type'] = 'text/plain'
         return version
 
 
-server_config: Dict[str, Dict] = {
+"""Establishing the connection to the DB."""
+config = configparser.RawConfigParser()
+here = os.path.dirname(__file__)
+config.read(os.path.join(here, 'sc3microapi.cfg'))
+
+# Read connection parameters
+host = config.get('mysql', 'host')
+user = config.get('mysql', 'user')
+password = config.get('mysql', 'password')
+db = config.get('mysql', 'db')
+
+conn = MySQLdb.connect(host, user, password, db)
+
+server_config = {
     'global': {
         'tools.proxy.on': True,
     	'server.socket_host': '127.0.0.1',
