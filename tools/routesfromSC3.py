acf70e7e62b6e5148eb22f918a8a30961985037d
Javier Quinteros <javier@quinteros.eu>
2020-02-10 16:11:36 +0100

    Add support for a rule file and change the output with that.

--- tools/routesfromSC3.py
+++ tools/routesfromSC3.py
@@ -28,6 +28,8 @@
 import sys
 import argparse
 import requests
+import xml.etree.cElementTree as ET
+import configparser
 
 
 def main():
@@ -38,14 +40,11 @@ def main():
     parser = argparse.ArgumentParser(description=msg)
     parser.add_argument('-o', '--output', default='routing.xml',
                         help='File to save the list of available routes.')
-    parser.add_argument('-r', '--rules',
-                        help='File with rules to generate the output.',
-                        default='rules.cfg')
+    parser.add_argument('-r', '--rules', default=None,
+                        help='File with rules to generate the output.')
     parser.add_argument('-u', '--url',
                         help='URL pointing to an instance of sc3microapi.',
                         default=urlbase)
-    parser.add_argument('-a', '--archive', type=str, default=None,
-                        help='Filter networks by its "archive" attribute. For instance, "GFZ".')
     parser.add_argument('-a', '--archive', type=str, default=None,
                         help='Filter networks by its "archive" attribute. For instance, "GFZ".')
     parser.add_argument('-s', '--shared', type=int, default=None, choices=[0, 1],
@@ -57,10 +56,9 @@ def main():
     args = parser.parse_args()
 
     # Call the sc3microapi method "networks"
-    url = '%s/network/'
+    url = '%s/network/' % args.url
 
-    # TODO Build query, read result and save to a file
-
+    # Build query, read result and save to a file
     headers = {
         'User-Agent': 'routesfromSC3 python-requests/' + requests.__version__,
     }
@@ -75,16 +73,39 @@ def main():
     if args.archive is not None:
         params['archive'] = args.archive
 
-    r = requests.get(url, params)
+    r = requests.get(url, params, headers=headers)
 
     if r.status_code != 200:
         print('Error reading from %s with parameters: %s' % (url, params))
         sys.exit(2)
 
+    # Filter and modify result based in file with rules
+
+    # Read networks to skip from a file with rules
+    if args.rules is not None:
+        config = configparser.RawConfigParser()
+        with open(args.rules, encoding='utf-8') as c:
+            config.read_file(c)
+
+        if config.has_section('Networks') and 'skip' in config.options('Networks'):
+            nets2skip = [x.strip() for x in config.get('Networks', 'skip').split(',')]
+
+    elem = ET.fromstring(r.content)
+    print('Before')
+    for net in elem:
+        print(net.attrib)
+        if net.get('networkCode') in nets2skip:
+            # FIXME This seems to modify the sequence while reading. Change!
+            elem.remove(net)
+            # print('Borro %s' % net.get('networkCode'))
+
+    print('After')
+    for net in elem:
+        print(net.attrib)
+
     with open(args.output, 'wb') as fout:
         fout.write(r.content)
 
-    # TODO Filter and modify result based in cfg file (e.g. station routes instead of network routes)
 
 
 if __name__ == '__main__':
