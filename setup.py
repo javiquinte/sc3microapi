78abfe6b73ce9bf3f57ec5610a93db7739bacad3
Javier Quinteros <javier@quinteros.eu>
2023-12-24 14:12:24 +0100

    Add uvicorn dependency

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'fastapi', 'pydantic>=2', 'mysqlclient'],
+    install_requires=['uvicorn', 'requests', 'fastapi', 'pydantic>=2', 'mysqlclient'],
 
     python_requires='>=3',
     # List additional groups of dependencies here (e.g. development
