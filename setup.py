057da1a4def7095a3463fe4f3b7c80fd3a283c20
Javier Quinteros <javier@quinteros.eu>
2023-12-24 05:10:04 +0100

    Add pydantic and mysqlclient as dependencies

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'cherrypy'],
+    install_requires=['requests', 'cherrypy', 'pydantic>=2', 'mysqlclient'],
 
     python_requires='>=3',
     # List additional groups of dependencies here (e.g. development
