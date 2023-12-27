3fff2ef2f4fb2d1b42b9e5d48610523b3f2be714
Javier Quinteros <javier@quinteros.eu>
2023-12-27 13:11:19 +0100

    Include method getstations to the classes accessing the DB

--- sc3microapi/db.py
+++ sc3microapi/db.py
@@ -6,7 +6,9 @@ from typing import Union
 from typing import Literal
 from typing import List
 from core import NetworkCode
+from core import StationCode
 from core import Network
+from core import Station
 from pydantic import conint
 import MySQLdb
 from MySQLdb.cursors import DictCursor
@@ -14,9 +16,16 @@ from MySQLdb.cursors import DictCursor
 
 class GenericConnection(ABC):
     @abstractmethod
-    def getnetworks(self, net: Union[NetworkCode, None], restricted: conint(ge=0, le=1) = None,
+    def getnetworks(self, net: Union[NetworkCode, None], restricted: Literal[0, 1] = None,
                     archive: str = None, netclass: Literal['p', 't'] = None,
-                    shared: conint(ge=0, le=1) = None, starttime: str = None, endtime: str = None) -> List[Network]:
+                    shared: Literal[0, 1] = None, starttime: datetime = None,
+                    endtime: datetime = None) -> List[Network]:
+        pass
+
+    @abstractmethod
+    def getstations(self, net: NetworkCode, sta: Union[StationCode, None], restricted: Literal[0, 1] = None,
+                    shared: Literal[0, 1] = None, starttime: datetime = None,
+                    endtime: datetime = None) -> List[Station]:
         pass
 
 
@@ -86,6 +95,74 @@ class TestConnection(GenericConnection):
         logging.debug(query % tuple(variables))
         return result
 
+    def getstations(self, net: NetworkCode, sta: Union[StationCode, None], restricted: Literal[0, 1] = None,
+                    shared: Literal[0, 1] = None, starttime: datetime = None,
+                    endtime: datetime = None) -> List[Station]:
+        query = ('select N.code as network, S.code as code, latitude, longitude, '
+                 'elevation, place, country, S.start, S.end, S.restricted, S.shared '
+                 'from Station as S join Network as N')
+        #         fields = ['network', 'code', 'latitude', 'longitude', 'elevation',
+        #                   'place', 'country', 'start', 'end', 'restricted', 'shared']
+        #         # fields.extend(self.extrafields)
+        #
+        whereclause = ['S._parent_oid=N._oid']
+        variables = []
+        if net[0] in '0123456789XYZ':
+            try:
+                net, year = net.split('_')
+            except ValueError:
+                # Send Error 400
+                errmsg = 'Wrong network code (%s). Temporary codes must include the start year (e.g. 4C_2011).' % net
+                raise Exception(errmsg % net)
+
+            whereclause.append('YEAR(N.start)=%s')
+            variables.append(int(year))
+
+        whereclause.append('N.code=%s')
+        variables.append(net)
+
+        if sta is not None:
+            result = [Station(**{'network': net, 'code': sta, 'latitude': 10.0, 'longitude': 20.0, 'elevation': 500.0,
+                                 'place': 'Some place in the world', 'country': 'Germany',
+                                 'start': datetime(2020, 1, 1),
+                                 'end': None, 'restricted': 0, 'shared': 1})]
+
+            whereclause.append('S.code=%s')
+            variables.append(sta)
+        else:
+            result = [Station(**{'network': net, 'code': 'STA01', 'latitude': 10.0, 'longitude': 20.0, 'elevation': 500.0,
+                                 'place': 'Some place in the world', 'country': 'Germany',
+                                 'start': datetime(2020, 1, 1),
+                                 'end': None, 'restricted': 0, 'shared': 1}),
+                      Station(**{'network': net, 'code': 'STA02', 'latitude': 10.0, 'longitude': 20.0, 'elevation': 500.0,
+                                 'place': 'Some place in the world', 'country': 'Germany',
+                                 'start': datetime(2020, 1, 1),
+                                 'end': datetime(2022, 1, 1), 'restricted': 1, 'shared': 0})
+                      ]
+
+        if restricted is not None:
+            whereclause.append('S.restricted=%s')
+            variables.append(restricted)
+
+        if shared is not None:
+            whereclause.append('S.shared=%s')
+            variables.append(shared)
+
+        if starttime is not None:
+            whereclause.append('S.start>=%s')
+            variables.append(starttime)
+
+        if endtime is not None:
+            whereclause.append('S.end<=%s')
+            variables.append(endtime)
+
+        if len(whereclause):
+            query = query + ' where ' + ' and '.join(whereclause)
+
+        # Complete SC3 data with local data
+        logging.debug(query % tuple(variables))
+        return result
+
 
 class SC3dbconnection(GenericConnection):
     def __init__(self, host: str, user: str, password: str, db: str = 'seiscomp3'):
@@ -109,9 +186,14 @@ class SC3dbconnection(GenericConnection):
         self.conn = MySQLdb.connect(self.host, self.user, self.password,
                                     self.db, cursorclass=DictCursor)
 
-    def getnetworks(self, net: Union[NetworkCode, None], restricted: conint(ge=0, le=1) = None,
+    def getnetworks(self, net: Union[NetworkCode, None], restricted: Literal[0, 1] = None,
                     archive: str = None, netclass: Literal['p', 't'] = None,
-                    shared: conint(ge=0, le=1) = None, starttime: str = None, endtime: str = None) -> List[Network]:
+                    shared: Literal[0, 1] = None, starttime: str = None, endtime: str = None) -> List[Network]:
+        pass
+
+    def getstations(self, net: NetworkCode, sta: Union[StationCode, None], restricted: Literal[0, 1] = None,
+                    shared: Literal[0, 1] = None, starttime: datetime = None,
+                    endtime: datetime = None) -> List[Station]:
         pass
 
     def fetchone(self):
