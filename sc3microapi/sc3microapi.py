74918074bca6a5f13d9196945c8f369e38a096e3
Javier Quinteros <javier@quinteros.eu>
2023-12-27 22:28:23 +0100

    Minor reorder of the methods in sc3microapi.py

--- sc3microapi/sc3microapi.py
+++ sc3microapi/sc3microapi.py
@@ -315,6 +315,23 @@ def basestation(net: NetworkCode, sta: StationCode = None, outformat: Literal['t
         return Response(content=''.join(outxml), media_type="application/xml")
 
 
+@app.get('/station/{net}', summary='Get list of stations', tags=['Station'])
+def getstations(net: NetworkCode, outformat: Literal['text', 'json', 'xml'] = 'json',
+               restricted: Literal[0, 1] = None, shared: Literal[0, 1] = None, starttime: Union[datetime, date] = None,
+               endtime: Union[datetime, date] = None):
+    #  -> Union[JSONResponse, PlainTextResponse, Response]
+    """Get information about one station in particular
+
+    - **net**: Network code
+    - **outformat**: Output format (json, text, xml)
+    - **restricted**: Restricted status of the Network (0 or 1)
+    - **shared**: Is the network shared with EIDA? (0 or 1)
+    - **starttime**: Start time in isoformat
+    - **endtime**: End time in isoformat
+    """
+    return basestation(net, None, outformat, restricted, shared, starttime, endtime)
+
+
 @app.get('/station/{net}/{sta}', summary='Get a station', tags=['Station'])
 def getstation(net: NetworkCode, sta: StationCode = None, outformat: Literal['text', 'json', 'xml'] = 'json',
                restricted: Literal[0, 1] = None, shared: Literal[0, 1] = None, starttime: Union[datetime, date] = None,
@@ -331,23 +348,6 @@ def getstation(net: NetworkCode, sta: StationCode = None, outformat: Literal['te
     - **endtime**: End time in isoformat
     """
     return basestation(net, sta, outformat, restricted, shared, starttime, endtime)
-
-
-@app.get('/station/{net}', summary='Get list of stations', tags=['Station'])
-def getstations(net: NetworkCode, outformat: Literal['text', 'json', 'xml'] = 'json',
-               restricted: Literal[0, 1] = None, shared: Literal[0, 1] = None, starttime: Union[datetime, date] = None,
-               endtime: Union[datetime, date] = None):
-    #  -> Union[JSONResponse, PlainTextResponse, Response]
-    """Get information about one station in particular
-
-    - **net**: Network code
-    - **outformat**: Output format (json, text, xml)
-    - **restricted**: Restricted status of the Network (0 or 1)
-    - **shared**: Is the network shared with EIDA? (0 or 1)
-    - **starttime**: Start time in isoformat
-    - **endtime**: End time in isoformat
-    """
-    return basestation(net, None, outformat, restricted, shared, starttime, endtime)
 
 # @cherrypy.expose
 # class AccessAPI(object):
@@ -528,226 +528,6 @@ def getstations(net: NetworkCode, outformat: Literal['text', 'json', 'xml'] = 'j
 #         # self.extrafields = extrafields.split(',') if len(extrafields) else []
 #         # self.stasuppl = configparser.RawConfigParser()
 #         # self.stasuppl.read('stations.cfg')
-#
-#
-# @cherrypy.expose
-# @cherrypy.popargs('net')
-# class NetworksAPI(object):
-#     """Object dispatching methods related to networks."""
-#
-#     def __init__(self, host: str, user: str, password: str, db: str):
-#         """Constructor of the NetworksAPI class."""
-#         # Save connection
-#         self.conn = SC3dbconnection(host, user, password, db)
-#         self.log = logging.getLogger('NetworksAPI')
-#
-#         # Get extra fields from the cfg file
-#         cfgfile = configparser.RawConfigParser()
-#         cfgfile.read('sc3microapi.cfg')
-#
-#         extrafields = cfgfile.get('Service', 'network', fallback='')
-#         self.extrafields = extrafields.split(',') if len(extrafields) else []
-#         self.netsuppl = configparser.RawConfigParser()
-#         self.netsuppl.read('networks.cfg')
-#
-#     @cherrypy.expose
-#     def index(self, net: NetworkCode = None, outformat: Literal['text', 'json', 'xml'] = 'json',
-#               restricted: Literal['0', '1'] = None, archive: str = None, netclass: Literal['p', 't'] = None,
-#               shared: Literal['0', '1'] = None, starttime: str = None, endtime: str = None, **kwargs) -> bytes:
-#         """List available networks in the system.
-#
-#         :param net: Network code
-#         :type net: str
-#         :param outformat: Output format (json, text, xml)
-#         :type outformat: str
-#         :param restricted: Restricted status of the Network ('0' or '1')
-#         :type restricted: str
-#         :param archive: Institution archiving the network
-#         :type archive: str
-#         :param netclass: Tpye of network (permanent 'p' or temporary 't')
-#         :type netclass: str
-#         :param shared: Is the network shared with EIDA? ('0' or '1')
-#         :type shared: str
-#         :param starttime: Start time in isoformat
-#         :type starttime: str
-#         :param endtime: End time in isoformat
-#         :type endtime: str
-#         :returns: Data related to the available networks.
-#         :rtype: utf-8 encoded string
-#         :raises: cherrypy.HTTPError
-#         """
-#
-#         if len(kwargs):
-#             # Send Error 400
-#             messdict = {'code': 0,
-#                         'message': 'Unknown parameter(s) "{}".'.format(kwargs.items())}
-#             message = json.dumps(messdict)
-#             self.log.error(message)
-#             raise cherrypy.HTTPError(400, message)
-#
-#         # Check parameters
-#         if restricted is not None:
-#             restricted = int(restricted)
-#             # except Exception:
-#             #     # Send Error 400
-#             #     messdict = {'code': 0,
-#             #                 'message': 'Restricted does not seem to be 0 or 1.'}
-#             #     message = json.dumps(messdict)
-#             #     self.log.error(message)
-#             #     raise cherrypy.HTTPError(400, message)
-#
-#         # Check parameters
-#         if shared is not None:
-#             shared = int(shared)
-#             # except Exception:
-#             #     # Send Error 400
-#             #     messdict = {'code': 0,
-#             #                 'message': 'Shared does not seem to be 0 or 1.'}
-#             #     message = json.dumps(messdict)
-#             #     self.log.error(message)
-#             #     raise cherrypy.HTTPError(400, message)
-#
-#         # if outformat not in ['json', 'text', 'xml']:
-#         #     # Send Error 400
-#         #     messdict = {'code': 0,
-#         #                 'message': 'Wrong value in the "outformat" parameter.'}
-#         #     message = json.dumps(messdict)
-#         #     self.log.error(message)
-#         #     raise cherrypy.HTTPError(400, message)
-#
-#         if starttime is not None:
-#             try:
-#                 str2date(starttime)
-#             except Exception:
-#                 # Send Error 400
-#                 messdict = {'code': 0,
-#                             'message': 'Error converting the "starttime" parameter (%s).' % starttime}
-#                 message = json.dumps(messdict)
-#                 self.log.error(message)
-#                 raise cherrypy.HTTPError(400, message)
-#
-#         if endtime is not None:
-#             try:
-#                 str2date(endtime)
-#             except Exception:
-#                 # Send Error 400
-#                 messdict = {'code': 0,
-#                             'message': 'Error converting the "endtime" parameter (%s).' % endtime}
-#                 message = json.dumps(messdict)
-#                 self.log.error(message)
-#                 raise cherrypy.HTTPError(400, message)
-#
-#         # try:
-#         query = 'select code, start, end, netClass, archive, restricted, shared from Network'
-#         fields = ['code', 'start', 'end', 'netClass', 'archive', 'restricted', 'shared']
-#         fields.extend(self.extrafields)
-#
-#         whereclause = []
-#         variables = []
-#         if net is not None:
-#             if net[0] in '0123456789XYZ':
-#                 try:
-#                     net, year = net.split('_')
-#                 except ValueError:
-#                     # Send Error 400
-#                     messdict = {'code': 0,
-#                                 'message': 'Wrong network code (%s). Temporary codes must include the start year (e.g. 4C_2011).' % net}
-#                     message = json.dumps(messdict)
-#                     self.log.error(message)
-#                     raise cherrypy.HTTPError(400, message)
-#
-#                 whereclause.append('YEAR(start)=%s')
-#                 variables.append(int(year))
-#
-#             whereclause.append('code=%s')
-#             variables.append(net)
-#
-#         if restricted is not None:
-#             whereclause.append('restricted=%s')
-#             variables.append(restricted)
-#
-#         if archive is not None:
-#             whereclause.append('archive=%s')
-#             variables.append(archive)
-#
-#         if netclass is not None:
-#             whereclause.append('netClass=%s')
-#             variables.append(netclass)
-#
-#         if shared is not None:
-#             whereclause.append('shared=%s')
-#             variables.append(shared)
-#
-#         if starttime is not None:
-#             whereclause.append('start>=%s')
-#             variables.append(starttime)
-#
-#         if endtime is not None:
-#             whereclause.append('end<=%s')
-#             variables.append(endtime)
-#
-#         if len(whereclause):
-#             query = query + ' where ' + ' and '.join(whereclause)
-#
-#         # self.cursor = self.conn.cursor()
-#         self.conn.execute(query, variables)
-#
-#         # Complete SC3 data with local data
-#         result = []
-#         curnet = self.conn.fetchone()
-#         while curnet:
-#             for field in self.extrafields:
-#                 curnet[field] = self.netsuppl.get(curnet['code'] + '-' + str(curnet['start'].year),
-#                                                   field, fallback=None)
-#             result.append(curnet)
-#             curnet = self.conn.fetchone()
-#
-#         if outformat == 'json':
-#             cherrypy.response.headers['Content-Type'] = 'application/json'
-#
-#             return json.dumps(result, default=datetime.datetime.isoformat).encode('utf-8')
-#         elif outformat == 'text':
-#             cherrypy.response.headers['Content-Type'] = 'text/plain'
-#
-#             fout = io.StringIO("")
-#             writer = csv.DictWriter(fout, fieldnames=fields, delimiter='|')
-#             writer.writeheader()
-#             writer.writerows(result)
-#             fout.seek(0)
-#             cherrypy.response.headers['Content-Type'] = 'text/plain'
-#             return fout.read().encode('utf-8')
-#         elif outformat == 'xml':
-#             cherrypy.response.headers['Content-Type'] = 'application/xml'
-#
-#             header = """<?xml version="1.0" encoding="utf-8"?>
-#   <ns0:routing xmlns:ns0="http://geofon.gfz-potsdam.de/ns/Routing/1.0/">
-#             """
-#             footer = """</ns0:routing>"""
-#
-#             outxml = [header]
-#             for net in result:
-#                 routetext = """
-#  <ns0:route networkCode="{netcode}" stationCode="*" locationCode="*" streamCode="*">
-#   <ns0:station address="https://geofon.gfz-potsdam.de/fdsnws/station/1/query" priority="1" start="{netstart}" end="{netend}" />
-#   <ns0:wfcatalog address="https://geofon.gfz-potsdam.de/eidaws/wfcatalog/1/query" priority="1" start="{netstart}" end="{netend}" />
-#   <ns0:dataselect address="https://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query" priority="1" start="{netstart}" end="{netend}" />
-#   <ns0:availability address="https://geofon.gfz-potsdam.de/fdsnws/availability/1/query" priority="1" start="{netstart}" end="{netend}" />
-#  </ns0:route>
-#  """
-#                 nc = net['code']
-#                 ns = net['start'].isoformat()
-#                 ne = net['end'].isoformat() if net['end'] is not None else ''
-#                 outxml.append(routetext.format(netcode=nc, netstart=ns, netend=ne))
-#             outxml.append(footer)
-#             return ''.join(outxml).encode('utf-8')
-#
-#         # except:
-#         #     # Send Error 404
-#         #     messDict = {'code': 0,
-#         #                 'message': 'Could not query the available networks'}
-#         #     message = json.dumps(messDict)
-#         #     self.log.error(message)
-#         #     raise cherrypy.HTTPError(404, message)
 #
 #
 # @cherrypy.expose
